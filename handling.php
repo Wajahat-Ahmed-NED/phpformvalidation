d7ed13aa7480fe9076f2106cf9341a9e6e407105
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-08-17 23:22:31 +0500

    Form Validate practice

---
+++ handling.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!-- get ki jaga post lagado to post se hojaye ga -->
+<form action="welcome.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<!-- get ki jaga post lagado to post se hojaye ga -->
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
