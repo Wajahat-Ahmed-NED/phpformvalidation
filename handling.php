dbb11278d1ee5170d41a930df744e81531fd09b1
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-08-22 13:20:25 +0500

    values attribute

--- handling.php
+++ handling.php
@@ -2,8 +2,8 @@
 <body>
 <!-- get ki jaga post lagado to post se hojaye ga -->
 <form action="welcome.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Name: <input type="text" name="name" value="<?php echo $name;?>"><br>
+E-mail: <input type="text" name="email" ><br>
 <input type="submit">
 </form>
 
